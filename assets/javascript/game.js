71aaf91382e469c45eb8126dd611394990a197f3
xer34 <40009885+xer34@users.noreply.github.com>
2018-09-16 20:39:58 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,6 +35,8 @@ $(function() {
 
 // loop to create four selectable answers that pull from answer array
 
+// when timer runs out check to see if correct answer is picked, display apprpriate page, restart timer and go to next page
+
 // if selected answer === correct answer, display win page
 
 // if selected answer != correct answer, display loss page
@@ -43,7 +45,9 @@ $(function() {
 
 // at the end display totals for incorrect and correct losses, percentage of correctly guessed
 
-// timers between each page
+
+
+
 
 
 
